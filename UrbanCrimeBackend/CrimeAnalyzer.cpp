6338fda8802f7684813359ad1b36bf57c5e6bfb9
MaxMil45 <mmurphy4562@gmail.com>
2025-07-07 15:10:18 +0100

    Add project files.

---
+++ UrbanCrimeBackend/CrimeAnalyzer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "CrimeAnalyzer running!" << std::endl;
+    // Your existing CrimeAnalyzer logic here
+
+    return 0;
+}
---
+++ UrbanCrimeFrontend/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace UrbanCrimeFrontend
+{
+    public partial class MainWindow : Window
+    {
+        private int clickCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LiveUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            clickCount++;
+            LiveTextBlock.Text = $"Button clicked {clickCount} time{(clickCount == 1 ? "" : "s")}";
+        }
+    }
+}
